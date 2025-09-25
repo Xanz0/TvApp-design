2751b95828b4a0f9aa1234ed0167821939f15b97
Xanz0 <davlataliqilichov@gmail.com>
2025-09-25 18:52:42 +0500

    head-link

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -1,103 +1,3 @@
-// import { useState } from "react";
-// import ReactCountryFlag from "react-country-flag";
-// import i18n from "i18next";
-// import "./designs/header.css";
-// import logo from "../../public/NYoTv.png";
-
-// const Header = () => {
-//   const [language, setLanguage] = useState("UZ");
-//   const [open, setOpen] = useState(false);
-
-//   const flags = {
-//     UZ: { code: "UZ", label: "O'zbekcha" },
-//     RU: { code: "RU", label: "Русский" },
-//     EN: { code: "GB", label: "English" }, 
-//   };
-
-//   return (
-//     <header className="header">
-//       <div className="container">
-//         <div className="header-wrapper">
-//           <div className="wrapper-top">
-//             <a href="/">
-//               <div className="top-left">
-//                 <img src={logo} className="logo-img" alt="Logo" />
-//                 <h3 className="logo-title"></h3>
-//               </div>
-//             </a>
-
-//             <div className="top-right">
-//               <ul className="nav-list">
-//                 <li className="nav-item"><a href="/">Bosh sahifa</a></li>
-//                 <li className="nav-item"><a href="/">Yangiliklar</a></li>
-//                 <li className="nav-item"><a href="/Motion">Biz Haqimizda</a></li>
-//                 <li className="nav-item"><a href="/progams">Teledasturlar</a></li>
-//                 <li className="nav-item"><a href="/">Ijodkorlar</a></li>
-
-//                 {/* 🌍 Dropdown til tanlash */}
-//                 <li className="nav-item language-dropdown">
-//                   <button
-//                     className="dropdown-toggle"
-//                     onClick={() => setOpen(!open)}
-//                   >
-//                     <ReactCountryFlag
-//                       countryCode={flags[language].code}
-//                       svg
-//                       style={{
-//                         width: "20px",
-//                         height: "15px",
-//                         borderRadius: "2px",
-//                       }}
-//                     />
-//                     <span className="flag-label">{flags[language].label}</span>
-//                     <span className="arrow">{open ? "▲" : "▼"}</span>
-//                   </button>
-
-//                   {open && (
-//                     <ul className="dropdown-menu">
-//                       {Object.keys(flags).map((key) => (
-//                         <li key={key}>
-//                           <button
-//                             className="dropdown-item"
-//                             onClick={() => {
-//                               setLanguage(key);
-//                               setOpen(false);
-//                             }}
-//                           >
-//                             <ReactCountryFlag
-//                               countryCode={flags[key].code}
-//                               svg
-//                               style={{
-//                                 width: "20px",
-//                                 height: "15px",
-//                                 borderRadius: "2px",
-//                               }}
-//                             />
-//                             <span>{flags[key].label}</span>
-//                           </button>
-//                         </li>
-//                       ))}
-//                     </ul>
-//                   )}
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
-
-
-
-
-
-
 import { useState } from "react";
 import ReactCountryFlag from "react-country-flag";
 import { useTranslation } from "react-i18next";
@@ -137,9 +37,9 @@ const Header = () => {
             <div className="top-right">
               <ul className="nav-list">
                 <li className="nav-item"><a href="/">{t("nav.home")}</a></li>
-                <li className="nav-item"><a href="/">{t("nav.news")}</a></li>
+                <li className="nav-item"><a href="/newsall">{t("nav.news")}</a></li>
                 <li className="nav-item"><a href="/Motion">{t("nav.about")}</a></li>
-                <li className="nav-item"><a href="/programs">{t("nav.programs")}</a></li>
+                <li className="nav-item"><a href="/tvprogramsall">{t("nav.programs")}</a></li>
                 <li className="nav-item"><a href="/">{t("nav.creators")}</a></li>
 
                 {/* 🌍 Dropdown til tanlash */}
